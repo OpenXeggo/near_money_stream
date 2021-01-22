85622d4a3bdc90919989b857daf607ebd389c3da
p2pvid <77237676+p2pvid@users.noreply.github.com>
2021-01-23 05:32:32 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,14 +1,29 @@
-const CONTRACT_NAME = process.env.CONTRACT_NAME ||'neardapp.testnet'
+const CONTRACT_NAME = process.env.CONTRACT_NAME ||'react-front'
 
 function getConfig(env) {
-	return {
-		networkId: 'testnet',
-		nodeUrl: 'https://rpc.testnet.near.org',
-		contractName: CONTRACT_NAME,
-		walletUrl: 'https://wallet.testnet.near.org',
-		helperUrl: 'https://helper.testnet.near.org',
-		explorerUrl: 'https://explorer.testnet.near.org',
-	}
+  switch (env) {
+
+  case 'production':
+  case 'mainnet':
+    return {
+      networkId: 'mainnet',
+      nodeUrl: 'https://rpc.mainnet.near.org',
+      contractName: CONTRACT_NAME,
+      walletUrl: 'https://wallet.near.org',
+      helperUrl: 'https://helper.mainnet.near.org',
+      explorerUrl: 'https://explorer.mainnet.near.org',
+    }
+  case 'development':
+  case 'testnet':
+    return {
+      networkId: 'testnet',
+      nodeUrl: 'https://rpc.testnet.near.org',
+      contractName: CONTRACT_NAME,
+      walletUrl: 'https://wallet.testnet.near.org',
+      helperUrl: 'https://helper.testnet.near.org',
+      explorerUrl: 'https://explorer.testnet.near.org',
+    }
+  }
 }
 
 module.exports = getConfig
